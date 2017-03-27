d64ecb24741e32ba60f87dc60b51196d1d8016c9
TechGIS <742311374@qq.com>
2017-03-27 10:55:35 +0800

    baiduMap offset processing
    In chinese：百度地图偏移量处理

--- ext/baidumap.js
+++ ext/baidumap.js
@@ -4,7 +4,10 @@
  * @Date 2017-3-23
  */
 import ol from 'openlayers';
-
+import projzh from 'projzh';
+/* projzh处理中文坐标的问题
+ *https://www.versioneye.com/nodejs/projzh/0.5.0
+ */
 ol.source.BaiduMap = function(options){
 	var options = options ? options : {};
 
@@ -15,13 +18,30 @@ ol.source.BaiduMap = function(options){
   		attributions = [ol.source.BaiduMap.ATTRIBUTION];
   	}
 
+    var extent = [72.004, 0.8293, 137.8347, 55.8271];
+
+    //定义百度坐标
+    //地址：https://github.com/openlayers/openlayers/issues/3522
+    var baiduMercator = new ol.proj.Projection({
+        code: 'baidu',
+        extent: ol.extent.applyTransform(extent, projzh.ll2bmerc),
+        units: 'm'
+    });
+    
+    ol.proj.addProjection(baiduMercator);
+    ol.proj.addCoordinateTransforms('EPSG:4326', baiduMercator, projzh.ll2bmerc, projzh.bmerc2ll);
+    ol.proj.addCoordinateTransforms('EPSG:3857', baiduMercator, projzh.smerc2bmerc, projzh.bmerc2smerc);
+
+
   	var resolutions = [];
     for(var i=0; i<19; i++){
         resolutions[i] = Math.pow(2, 18-i);
     }
     var tilegrid  = new ol.tilegrid.TileGrid({
         origin: [0,0],
-        resolutions: resolutions
+        resolutions: resolutions,
+        extent: ol.extent.applyTransform(extent, projzh.ll2bmerc),
+        tileSize: [256, 256]
     });
 
   	/*var url = options.url  !== undefined ?
@@ -29,7 +49,8 @@ ol.source.BaiduMap = function(options){
   	ol.source.TileImage.call(this, {
   		crossOrigin: 'anonymous',   //跨域
 	    cacheSize: options.cacheSize,
-  		projection: ol.proj.get('EPSG:3857'),
+        // projection: ol.proj.get('EPSG:3857'),
+  		projection:'baidu',
   		tileGrid: tilegrid,
   		tileUrlFunction: function(tileCoord, pixelRatio, proj){
             if(!tileCoord) return "";
